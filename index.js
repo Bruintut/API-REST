50f52fab5b9b803a436503525606a8971567e16f
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-22 20:26:24 -0300

    feat: config CORS

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const port = 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }),
+);
 
 app.use(express.json());
 
