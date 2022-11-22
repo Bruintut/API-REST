36dc63a0fd38d8532a2f4e0464ce000385164512
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-22 20:08:08 -0300

    feat: add nodemon

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello');
+app.use(express.json());
+
+const herois = [
+  {
+    id: 1,
+    nome: 'Izuku Midoriya',
+    descricao:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    foto: './assets/images/izuku.jpg',
+    idade: 16,
+  },
+];
+
+app.get('/herois/find-herois', function (req, res) {
+  res.send(herois);
+});
+
+app.get('/herois/find-heroi/:id', (req, res) => {
+  const idParam = req.params.id;
+  const chosenHeroi = herois.find((heroi) => heroi.id == idParam);
+  res.send(chosenHeroi);
 });
 
 app.listen(port, () => {
